80c1b07d62027dc9393a6edc51daad1ea6f5d0cd
abdulomeiza <abdullahiomeiza12@gmail.com>
2023-08-12 15:16:46 +0000

    removing sourcestage

--- sourceStage.ts
+++
@@ -1,6 +0,0 @@
-       //Source Stage
-        const sourceStage = new SourceStage(this);
-        codepipeline.addStage({
-            stageName: "Source",
-            actions: [sourceStage.getCodeCommitSourceAction()],
-        });
